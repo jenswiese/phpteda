47c88a91127f65aeac4d7feb6f3cab977eaf1824
Jens Wiese <jens@dev.lohering.de>
2013-04-08 12:53:15 +0200

    Removed required composer to access ConsoleIO, impl. adapted version of it

--- src/Phpteda/CLI/IO/ConsoleIO.php
+++ src/Phpteda/CLI/IO/ConsoleIO.php
@@ -2,16 +2,42 @@
 
 namespace Phpteda\CLI\IO;
 
-use Composer\IO\ConsoleIO as ComposerIO;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class ConsoleIO
+ * Class to access ConsoleIO
  *
- * @author jens
+ * (heavily inspired by Composer\IO, https://github.com/composer/composer)
+ *
+ * @author Jens Wiese <jens@howtrueisfalse.de>
  * @since 2013-04-07
  */
-class ConsoleIO extends ComposerIO
+class ConsoleIO
 {
+    /** @var \Symfony\Component\Console\Input\InputInterface */
+    protected $input;
+
+    /** @var \Symfony\Component\Console\Output\OutputInterface */
+    protected $output;
+
+    /** @var \Symfony\Component\Console\Helper\HelperSet */
+    protected $helperSet;
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param HelperSet $helperSet
+     */
+    public function __construct(InputInterface $input, OutputInterface $output, HelperSet $helperSet)
+    {
+        $this->input = $input;
+        $this->output = $output;
+        $this->helperSet = $helperSet;
+    }
+
+
     /**
      * @param string $name
      * @param mixed $value
@@ -28,6 +54,53 @@ class ConsoleIO extends ComposerIO
     public function getArgument($name)
     {
         return $this->input->getArgument($name);
+    }
+
+    /**
+     * @param string $messages
+     * @param bool $newline
+     */
+    public function write($messages, $newline = true)
+    {
+        $this->output->write($messages, $newline);
+    }
+
+    /**
+     * @param string $question
+     * @param mixed $default
+     * @return string The answer
+     */
+    public function ask($question, $default = null)
+    {
+        return $this->helperSet->get('dialog')->ask($this->output, $question, $default);
+    }
+
+    /**
+     * @param string $question
+     * @param mixed $default
+     * @return string The answer
+     */
+    public function askConfirmation($question, $default = true)
+    {
+        return $this->helperSet->get('dialog')->askConfirmation($this->output, $question, $default);
+    }
+
+    /**
+     * @param string $question
+     * @param $validator
+     * @param bool $attempts
+     * @param mixed $default
+     * @return string The answer
+     */
+    public function askAndValidate($question, $validator, $attempts = false, $default = null)
+    {
+        return $this->helperSet->get('dialog')->askAndValidate(
+            $this->output,
+            $question,
+            $validator,
+            $attempts,
+            $default
+        );
     }
 
     /**
